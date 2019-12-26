ac64b5d60a252b420237a5bb4d460cacd0880b27
Arunkumra <priyadharsini559955@gmail.com>
2019-12-26 12:40:42 +0530

    new pack

---
+++ src/org/sampless/WebBrowser.java
@@ -0,0 +1,15 @@
+package org.sampless;
+
+public class WebBrowser {
+public void river() {
+	System.out.println("Kavery");
+}
+public void sea() {
+	System.out.println("Indian Ocean");
+}
+public static void main(String[] args) {
+	WebBrowser a=new WebBrowser();
+	a.sea();
+	a.river();
+}
+}
